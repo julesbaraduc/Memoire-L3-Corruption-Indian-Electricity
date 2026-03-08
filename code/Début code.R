1914a2f580582ab32cfd8192c15921011e3575c8
julesbaraduc <262400522+julesbaraduc@users.noreply.github.com>
2026-03-08 16:29:52 +0100

    correction avant le pull

--- code/Début code.R
+++ code/Début code.R
@@ -4,12 +4,12 @@ library(tidyverse)
 
 load("Billing_Data.RData")
 load("shrug_triv_candidate.RData")
+load("shrug_triv_elections_clean.RData")
 load("shrug-dmsp.RData")
 load("shrug-viirs-annual.Rdata")
 load("Shurg_con_key.RData")
 
+
 # test fun : moyenne_lumiere <- mean(dsmp_shrid$dmsp_mean_light, na.rm = TRUE)
 
 head(dsmp_con07)
-
-
